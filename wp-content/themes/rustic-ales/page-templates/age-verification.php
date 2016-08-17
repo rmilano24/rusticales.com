0a746ebeb2cacb5b9e77ea0e7a081778ce604529
jonas-davidson <jonasd1977@gmail.com>
2016-08-17 16:30:23 -0700

    Adjusted href for 'No' option on age verification to send to google images for legos

--- wp-content/themes/rustic-ales/page-templates/age-verification.php
+++ wp-content/themes/rustic-ales/page-templates/age-verification.php
@@ -9,7 +9,7 @@
       <div class="container y-paddings" id="age-verification">
       <h1>Are you over 21 years of age?</h1>
       <a href="#" id="close-verification">Yes</a>
-      <a href="http://www.pokemon.com/us/">No</a>
+      <a href="https://www.google.com/search?q=legos&hl=en&biw=1291&bih=1316&site=webhp&source=lnms&tbm=isch&sa=X&ved=0ahUKEwi59ff6zMnOAhXCGR4KHePZDQcQ_AUIBygC">No</a>
       </div>
       
 	<?php get_footer(); ?>
