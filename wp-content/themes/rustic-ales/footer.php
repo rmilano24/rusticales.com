5c754099a007379cd71922c764e87ce4f06bdb39
jonas-davidson <jonasd1977@gmail.com>
2016-12-30 10:09:35 -0800

    Adjusted animation of sub navs to prevent two open at once

--- wp-content/themes/rustic-ales/footer.php
+++ wp-content/themes/rustic-ales/footer.php
@@ -103,7 +103,7 @@
           $('#main-nav ul li.menu-item-has-children').on('mouseenter', function () {
           $(this).children('ul').fadeIn();  
           }).on('mouseleave', function () {
-            $(this).children('ul').delay(400).fadeOut();
+            $(this).children('ul').hide();
         });
 
         // Directional Hover Effects
