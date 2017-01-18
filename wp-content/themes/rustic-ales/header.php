75b12b5448104ff66154cfc927a4554515c88cb6
jonas-davidson <jonasd1977@gmail.com>
2017-01-17 17:37:29 -0800

    Added favicon

--- wp-content/themes/rustic-ales/header.php
+++ wp-content/themes/rustic-ales/header.php
@@ -14,7 +14,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title><?php wp_title(); ?></title>
-    
+    <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
+    <link rel="icon" href="/favicon.ico" type="image/x-icon">
 	<?php
 	  /*
 	   * We add some JavaScript to pages with the comment form
